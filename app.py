255657973f77bd06a6a471ae384afde7a20572be
Sagar Neupane <cyber.crime252@gmail.com>
2022-10-11 19:09:17 +0545

    Updating error_page variable

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 
+from tracemalloc import start
 import requests 
 from os.path import exists as file_exists
 
-
-def create_names(search_data):
-    error_file_name = f"{search_data}_error_page.txt"
-    data_file_name = f"{search_data}.json"
-    
-    return error_file_name, data_file_name
 
 # Creating unique file name according to search result.
 # this is the file where the pages with error are stored.
@@ -19,22 +14,27 @@ def create_names(search_data):
 # this will take as if there are no error pages
 
 def read_error_page(error_file_name):
-    error_pages_list = []
+    error_page = None
     if file_exists(error_file_name):
 
         with open(error_file_name, "r+", encoding="utf-8") as f:
             datas = f.read()
             print(datas)
             for data in datas:
-                error_pages_list.append(int(data))
-    return error_pages_list
+                error_page = int(data)
+    return error_page
 
 
 # Into The page changes. The given loop will go upto 1-10 pages.
-def search_data_from_url(search_data,error_pages_list):
+def search_data_from_url(search_data,error_page):
     articles = {
     }
-    for i in range(1,11):
+    if error_page is None:
+        start_page = 1
+    else:
+        start_page = error_page + 1
+    
+    for i in range(start_page,start_page+3):
 
         payload = {
             "title":search_data,
@@ -43,24 +43,25 @@ def search_data_from_url(search_data,error_pages_list):
         # Checking if the page number exists in the error_pages list.
         # If it exists it means the search page has some errors so ignoring that 
         # If it doesnot then keep on scrapping the data
-        if i not in error_pages_list:
-            r = requests.get("https://bg.annapurnapost.com/api/search", params=payload)
-            print("visited")
-            if r.status_code == 200:
-                data = r.json()
-                for item in data['data']['items']:
-                    key_name = f"article_{item['id']}"
-                    articles[key_name] = {
-                        "title":item["title"],
-                        "author":item["author"],
-                        "content":item["content"],
-                    }
+        
+        r = requests.get("https://bg.annapurnapost.com/api/search", params=payload)
+        print("visited")
+        if r.status_code == 200:
+            data = r.json()
+            for item in data['data']['items']:
+                key_name = f"article_{item['id']}"
+                articles[key_name] = {
+                    "title":item["title"],
+                    "author":item["author"],
+                    "content":item["content"],
+                }
 
-            else:
-                error_pages_list.append(i)
-                break
+        else:
+            error_page = i
+            break
+
     
-    return error_pages_list, articles
+    return error_page, articles
 
 
 # # Giving the file name to store the scrap data.
@@ -68,10 +69,9 @@ def search_data_from_url(search_data,error_pages_list):
 
 
 # Writing the error page value into the file so that next time we can ignore that page
-def create_error_file(error_file_name, error_pages_list):
+def create_error_file(error_file_name, error_page):
     with open(error_file_name, "w+") as f:
-        for data in error_pages_list:
-            f.write(str(data))
+        f.write(str(error_page))
 
 
 # Writing the scrapped data which was in form of dictonary into a json file.
@@ -91,12 +91,14 @@ search_data = "नेपालि"
 # search_data = "मरभुमि"
 
 
-error_file_name, data_file_name = create_names(search_data)
 
-error_pages_list = read_error_page(error_file_name)
+error_file_name = f"{search_data}_error_page.txt"
+error_page = read_error_page(error_file_name)
 
-updated_error_pages , data_to_store = search_data_from_url(search_data,error_pages_list)
+data_file_name = f"{search_data}_{error_page}.json"
 
-create_error_file(error_file_name,updated_error_pages)
+updated_error_page , data_to_store = search_data_from_url(search_data,error_page)
+
+create_error_file(error_file_name,updated_error_page)
 # print(data_to_store)
 create_json_data(data_file_name,data_to_store)
